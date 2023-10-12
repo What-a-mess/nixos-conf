192a195f4e1009941b28e2667bc0833379d4b7d8
wamess <44860987+What-a-mess@users.noreply.github.com>
2023-10-12 16:00:36 +0800

    some update

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -2,6 +2,12 @@
   imports = [
     ../utils/waybar
     ../utils/wofi
+    ../utils/mako
+    ../utils/wl-clipboard
+  ];
+  home.packages = with pkgs; [
+    hyprpaper
+    mako
   ];
 
   # home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
