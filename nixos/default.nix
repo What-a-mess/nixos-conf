959caa8bfd2f2830f6aa68a908cf8c908a2362c2
wamess <44860987+What-a-mess@users.noreply.github.com>
2024-02-19 21:47:08 +0800

    test

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,9 +1,7 @@
 {
-  imports = [
-    ./environment.nix
-    ./user.nix
-    ./audio.nix
-    ./fonts.nix
-    ./desktop/kde.nix
-  ];
+  environment = import ./environment.nix;
+  user = import ./user.nix;
+  audio = import ./audio.nix;
+  fonts = import ./fonts.nix;
+  desktop = import ./desktop/kde.nix;
 }
