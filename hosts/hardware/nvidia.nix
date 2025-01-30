dafc84378f8def1f82a92b9c23964720f1e9171e
wamess <44860987+What-a-mess@users.noreply.github.com>
2025-01-30 14:15:48 +0800

    more fix

--- hosts/hardware/nvidia.nix
+++ hosts/hardware/nvidia.nix
@@ -35,6 +35,7 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    open = true;
   };
 }
 
